26eaa1471170c24eaca163672b0cc8c3bc2bb1bb
Arthur Picard <arthur.picard7@gmail.com>
2021-12-16 19:42:43 +0100

    Article component created

---
+++ lib/01_article.jsx
@@ -0,0 +1,15 @@
+// TODO: import the right dependencies
+import React, { Component } from 'react';
+// TODO: code the Article component
+class Article extends Component {
+  render(){
+    return (
+      <div>
+        <h2>My fancy new product</h2>
+        <p>Test it now!</p>
+      </div>
+    );
+  }
+}
+
+export default Article;
